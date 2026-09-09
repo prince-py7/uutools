67fac238592e64c0c030ac71b87b726fa43f1cf9
prince-py7 <118663194+prince-py7@users.noreply.github.com>
2026-09-09 22:28:19 +0530

    Update IMAGE_API_URL to the correct endpoint

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Example: "https://your-api.com/profile"
 // Site UUID ko ?uuid= ke saath bhejegi
 // ===============================
-const IMAGE_API_URL = "PASTE_YOUR_API_URL_HERE";
+const IMAGE_API_URL = "https://api.uuonline.in/api/student/image/";
