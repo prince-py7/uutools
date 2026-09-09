5d24f99e7398941eca130fd8f5c36ef2a4b982eb
prince-py7 <118663194+prince-py7@users.noreply.github.com>
2026-09-09 23:53:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // File: config.js
 // Final request: https://api.uuonline.in/api/xyz/image/USER_UUID
 // ===============================
-const IMAGE_API_URL = "https://api.uuonline.in/api/xyz/image";
+const IMAGE_API_URL = "https://api.uuonline.in/api/student/image";
